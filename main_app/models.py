c2d36e7d639541d0ad4adf396241a2657ac8109a
Dyv Lee <d1j3l@Dyvs-MacBook-Pro.local>
2022-01-21 13:40:03 -0800

    Completed last step

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.forms import IntegerField
 
 # Create your models here.
 class Widget(models.Model):
     description = models.CharField(max_length=50)
-    quantity = IntegerField()
+    quantity = models.IntegerField()
 
     def __str__(self):
         return self.item
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Widget
-
+from .forms import WidgetForm
 # Create your views here.
+
 def index(request):
+    widget_form = WidgetForm()
     widgets = Widget.objects.all()
-    return render(request, 'index.html', {'widgets': widgets})
+    return render(request, 'index.html', {'widgets': widgets, 'widget_form': widget_form})
+
+
+def add_widget(request):
+    form = WidgetForm(request.POST)
+    form.save()
+    return redirect('/')
+
+def delete_widget(request, id):
+    Widget.objects.get(id=id).delete()
+    return redirect('/')
